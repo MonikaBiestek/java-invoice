90d589c4d5cb771423a7b7f3514b7399d544fc3f
Monika Biestek <Monika Biestek>
2020-05-19 23:44:31 +0200

    indentation corrected

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -8,70 +8,71 @@ import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
 
-	private Map<Product, Integer> products = new HashMap<>();
+    private Map<Product, Integer> products = new HashMap<>();
 
-	private static int nextNumber = 0;
+    private static int nextNumber = 0;
 
-	private final int number = ++nextNumber;
+    private final int number = ++nextNumber;
 
-	public void addProduct(Product product) {
-		addProduct(product, 1);
-	}
+    public void addProduct(Product product) {
+        addProduct(product, 1);
+    }
 
-	public Map<Product, Integer> getProducts() {
-		return products;
-	}
+    public Map<Product, Integer> getProducts() {
+        return products;
+    }
 
-	public void addProduct(Product product, Integer quantity) {
-		if (product == null || quantity <= 0) {
-			throw new IllegalArgumentException();
-		} else if (products.containsKey(product)) {
-			products.put(product, quantity + products.get(product));
-		} else {
-			products.put(product, quantity);
-		}
-	}
+    public void addProduct(Product product, Integer quantity) {
+        if (product == null || quantity <= 0) {
+            throw new IllegalArgumentException();
+        } else if (products.containsKey(product)) {
+            products.put(product, quantity + products.get(product));
+        } else {
+            products.put(product, quantity);
+        }
+    }
 
-	public BigDecimal getNetTotal() {
-		BigDecimal totalNet = BigDecimal.ZERO;
-		for (Product product : products.keySet()) {
-			BigDecimal quantity = new BigDecimal(products.get(product));
-			totalNet = totalNet.add(product.getPrice().multiply(quantity));
-		}
-		return totalNet;
-	}
+    public BigDecimal getNetTotal() {
+        BigDecimal totalNet = BigDecimal.ZERO;
+        for (Product product : products.keySet()) {
+            BigDecimal quantity = new BigDecimal(products.get(product));
+            totalNet = totalNet.add(product.getPrice().multiply(quantity));
+        }
+        return totalNet;
+    }
 
-	public BigDecimal getTaxTotal() {
-		return getGrossTotal().subtract(getNetTotal());
-	}
+    public BigDecimal getTaxTotal() {
+        return getGrossTotal().subtract(getNetTotal());
+    }
 
-	public BigDecimal getGrossTotal() {
-		BigDecimal totalGross = BigDecimal.ZERO;
-		for (Product product : products.keySet()) {
-			BigDecimal quantity = new BigDecimal(products.get(product));
-			totalGross = totalGross.add(product.getPriceWithTax().multiply(quantity));
-		}
-		return totalGross;
-	}
+    public BigDecimal getGrossTotal() {
+        BigDecimal totalGross = BigDecimal.ZERO;
+        for (Product product : products.keySet()) {
+            BigDecimal quantity = new BigDecimal(products.get(product));
+            totalGross = totalGross.add(product.getPriceWithTax().multiply(quantity));
+        }
+        return totalGross;
+    }
 
-	public int getNumber() {
-		return number;
-	}
+    public int getNumber() {
+        return number;
+    }
 
-	public void printInvoice() {
-		ArrayList<String> lines = prepareInvoice();
-		for (int i = 0; i < lines.size(); i++) {
-			System.out.println(lines.get(i));
-		}
-	}
+    public void printInvoice() {
+        ArrayList<String> lines = prepareInvoice();
+        for (int i = 0; i < lines.size(); i++) {
+            System.out.println(lines.get(i));
+        }
+    }
 
-	public ArrayList<String> prepareInvoice() {
-		ArrayList<String> lines = new ArrayList<String>();
-		lines.add("Numer faktury: " + String.valueOf(getNumber()));
-		for (Product p : products.keySet()) {
-			lines.add("Nazwa: " + p.getName() + " " + "Cena: " + String.valueOf(p.getPrice()) + " " + "Liczba sztuk: " + products.get(p));
-		}
-		lines.add("Ilość pozycji na fakturze: " + Integer.toString(products.size()));
-		return lines;
-	}
+    public ArrayList<String> prepareInvoice() {
+        ArrayList<String> lines = new ArrayList<String>();
+        lines.add("Numer faktury: " + String.valueOf(getNumber()));
+        for (Product p : products.keySet()) {
+            lines.add("Nazwa: " + p.getName() + " " + "Cena: " + String.valueOf(p.getPrice()) + " " + "Liczba sztuk: "
+                    + products.get(p));
+        }
+        lines.add("Ilość pozycji na fakturze: " + Integer.toString(products.size()));
+        return lines;
+    }
 }
--- src/main/java/pl/edu/agh/mwo/invoice/product/ExciseProduct.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/ExciseProduct.java
@@ -3,14 +3,14 @@ package pl.edu.agh.mwo.invoice.product;
 import java.math.BigDecimal;
 
 public abstract class ExciseProduct extends Product {
-	private BigDecimal excise = new BigDecimal("5.56");
+    private BigDecimal excise = new BigDecimal("5.56");
 
-	public ExciseProduct(String name, BigDecimal price, BigDecimal tax) {
-		super(name, price, tax);
-	}
+    public ExciseProduct(String name, BigDecimal price, BigDecimal tax) {
+        super(name, price, tax);
+    }
 
-	public BigDecimal getPriceWithTax() {
-		return super.getPriceWithTax().add(excise);
-	}
+    public BigDecimal getPriceWithTax() {
+        return super.getPriceWithTax().add(excise);
+    }
 
 }
